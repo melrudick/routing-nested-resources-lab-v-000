7e1f2c725f66243c99329621d650a444b2ccd9d7
Mel Rudick <melissarudick@MacBook-Air.local>
2018-04-11 18:09:00 -0500

    trying to get alert to appear

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
+      if !@artist
         redirect_to artists_path, alert: "Artist not found"
       else
         @songs = @artist.songs
@@ -15,16 +15,16 @@ class SongsController < ApplicationController
   def show
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
-      else
-        @song = Song.find_by(id: params[:id])
-      end
+      # if @artist.nil?
+      #   redirect_to artists_path, alert: "Artist not found"
+      # else
+        @song = @artist.songs.find_by(id: params[:id])
+      # end
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
     else
-      @song = Song.find_by(id: params[:id])
+      @song = Song.find(params[:id])
     end
   end
 
